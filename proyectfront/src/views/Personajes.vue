09fbff841485380cf52cdf984462b007469c98e3
AlanMC7650 <alan.maldonado@ucb.edu.bo>
2025-05-12 14:51:45 -0400

    commit 1 Jhon

--- proyectfront/src/views/Personajes.vue
+++ proyectfront/src/views/Personajes.vue
@@ -27,9 +27,9 @@ onMounted(async () => {
 </script> -->
 
 <template>
-  <div class="container mt-4">
-    <h2>Personajes</h2>
-    <table class="table table-striped">
+  <div class="container psnj">
+    <h2 class="Tp">Personajes</h2>
+    <table class="Tps">
       <thead>
         <tr>
           <th>Nombre</th>
@@ -61,3 +61,48 @@ onMounted(async () => {
   personajes.value = data
 })
 </script>
+<style scoped>
+.psnj{
+  max-width: 1000px;
+  margin: 2rem auto;
+  background-color: #fff;
+  padding: 2rem;
+  border-radius: 6px;
+  box-shadow: 0 2px 15px rgba(0, 0, 0, 0.1);
+}
+
+.Tp {
+  text-align: center;
+  font-size: 2rem;
+  margin-bottom: 1.5rem;
+  color: #8b0000;
+}
+
+.Tps {
+  width: 100%;
+  border-collapse: collapse;
+  text-align: left;
+  background-color: #fafafa;
+}
+
+.Tps thead {
+  background-color: #8b0000;
+  color: #fff;
+}
+
+.Tps th,
+.Tps td {
+  padding: 12px 15px;
+  border: 1px solid #ddd;
+}
+
+.Tps tr:nth-child(even) {
+  background-color: #f9f9f9;
+}
+
+.Tps tr:hover {
+  background-color: #670000;
+    color: #fff;
+}
+</style>
+ 
